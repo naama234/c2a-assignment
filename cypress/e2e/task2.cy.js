1377fcd5b77dd5881f6167d9a2193ea620936ebc
naama234 <naama234@gmail.com>
2023-08-08 15:39:56 +0300

    fix tests

--- cypress/e2e/task2.cy.js
+++ cypress/e2e/task2.cy.js
@@ -6,9 +6,7 @@ describe('Compare PDF Files Test', () => {
     const pdf2Path = path.join('cypress', 'fixtures', 'pdfs' ,'dummy2.pdf');
 
     cy.task('readPdf', pdf1Path).then((pdf1Text) => {
-      // Read the text from the second PDF file
       cy.task('readPdf', pdf2Path).then((pdf2Text) => {
-        // Compare the texts of the two PDF files
         expect(pdf1Text).to.equal(pdf2Text);
       });
     });
